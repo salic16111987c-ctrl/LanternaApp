55161b6745eeb85c21208106882b0ff9d42f824f
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-15 14:15:39 -0300

    Configurar aplicativo Caixa da Loja v2.3

---
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.techcell.caixadaloja"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.techcell.caixadaloja"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 5
+        versionName = "2.3"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
