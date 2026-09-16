3f36be93811161b85655705803c533f06046e48f
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-15 22:17:44 -0300

    Atualizar versão Caixa da Loja para 2.6

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techcell.caixadaloja"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.5"
+        versionCode = 8
+        versionName = "2.6"
     }
 
     buildTypes {
