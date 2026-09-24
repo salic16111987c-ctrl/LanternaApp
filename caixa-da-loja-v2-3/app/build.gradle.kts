bcefe6eccaeac24f85d7f5b30e18115dc410cc57
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-24 17:29:40 -0300

    Bump Tech Cell ACS alpha2

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techcell.techcellacs.alpha"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "3.0.0-alpha1"
+        versionCode = 2
+        versionName = "3.0.0-alpha2"
     }
 
     buildTypes {
