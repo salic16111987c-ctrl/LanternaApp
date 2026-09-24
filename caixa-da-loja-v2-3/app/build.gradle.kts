5dcde4aa65391846c37a77329f5dfe0185680ed5
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-24 17:47:49 -0300

    Use stable isolated package for Gestão test builds

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.techcell.techcellacs.alpha"
+        applicationId = "com.techcell.techcellacs.gestao"
         minSdk = 24
         targetSdk = 35
         versionCode = 3
