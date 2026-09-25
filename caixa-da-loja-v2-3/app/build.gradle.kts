3ae414486997dd8747013dce099bddf610534a49
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-25 19:04:52 -0300

    Bump PDV Preview to Alpha 16

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techcell.techcellacs.gestao.pdvpreview"
         minSdk = 24
         targetSdk = 35
-        versionCode = 150
-        versionName = "3.0.0-alpha15-pdvpreview"
+        versionCode = 160
+        versionName = "3.0.0-alpha16-pdvpreview"
     }
 
     buildTypes {
