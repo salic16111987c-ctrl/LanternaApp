a58dec05867b27a03ec5b72e48c8eb2347dc2c7f
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-25 20:11:35 -0300

    Bump preview to Alpha 20

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techcell.techcellacs.gestao.pdvpreview"
         minSdk = 24
         targetSdk = 35
-        versionCode = 190
-        versionName = "3.0.0-alpha19-pdvpreview"
+        versionCode = 200
+        versionName = "3.0.0-alpha20-pdvpreview"
     }
 
     buildTypes {
