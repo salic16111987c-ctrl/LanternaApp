ef8e7d07089f12b716384a7b6801da7a96a74e87
salic16111987c-ctrl <salic16111987c@gmail.com>
2026-09-26 10:42:34 -0300

    Bump preview to Alpha 24

--- caixa-da-loja-v2-3/app/build.gradle.kts
+++ caixa-da-loja-v2-3/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techcell.techcellacs.gestao.pdvpreview"
         minSdk = 24
         targetSdk = 35
-        versionCode = 230
-        versionName = "3.0.0-alpha23-pdvpreview"
+        versionCode = 240
+        versionName = "3.0.0-alpha24-pdvpreview"
     }
 
     buildTypes {
